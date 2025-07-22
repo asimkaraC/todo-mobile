33b9a988d989be344e0fbb63d7e455ba8959f942
Asim Kara <akara4314@conestogac.on.ca>
2025-07-22 14:05:41 -0400

    Added auth to firebase config

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDmEldpquSTqyGOtLLramX50wABpVywrLU",
   authDomain: "todo-app-381f7.firebaseapp.com",
@@ -17,6 +14,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { app, db };
+export { app, auth, db };
 
